676f33542ac75f33f840ab024f9dafcfe96ab8b7
Dimas Prasetio <dprasetio6@gmail.com>
2022-12-14 11:05:13 +0700

    update nina

--- routes/nina.js
+++ routes/nina.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 const nina = require("./controllers/nina.controller.js");
 
-router.post("/get_nina", nina.get_nina);
 router.post("/get_selected_nina_dummy", nina.get_selected_nina_dummy);
 
 
