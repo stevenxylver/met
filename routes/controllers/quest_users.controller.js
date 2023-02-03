3e20cf21a87751acea32529320535b02237e806d
Dimas Prasetio <dprasetio6@gmail.com>
2023-02-03 13:55:58 +0700

    update api complete quest

--- routes/controllers/quest_users.controller.js
+++ routes/controllers/quest_users.controller.js
@@ -40,3 +40,59 @@ exports.get_specific_quests = async (req, res) => {
         });
     }
 };
+
+exports.complete_quests = async (req, res) => {
+    try {
+        const quests = await Quests.findOne({
+            where: {
+                id: req.body.quest_id,
+            },
+        });
+
+        const questUsers = await QuestUsers.findOne({
+            where: {
+                quest_id: req.body.quest_id,
+                user_id: req.body.user_id,
+            },
+        })
+        .then(async data => {
+            if (data) {
+                const body = {
+                  status: 'Complete',
+                };
+                return await data.update(body);
+            }
+        })
+
+        const users = await Users.findOne({
+            where: {
+                id: req.body.user_id,
+            },
+        })
+        .then(async data => {
+            if (data) {
+                const userToken = parseFloat(data.token);
+                const rewardToken = parseFloat(quests.reward_token);
+                const finalToken = userToken + rewardToken;
+
+                const body = {
+                  token: finalToken,
+                };
+                return await data.update(body);
+            }
+        });
+
+        res.send({
+            success : true,
+            data: questUsers,
+            message : "Quest complete"
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            success : false,
+            data    : null,
+            message : error
+        });
+    }
+};
--- routes/quests.js
+++ routes/quests.js
@@ -7,5 +7,6 @@ const quest_users = require("./controllers/quest_users.controller.js");
 router.get("/all", quests.findAll);
 router.post("/get_specific_quests", quests.get_specific_quests);
 router.post("/users", quest_users.get_specific_quests);
+router.post("/complete-quests", quest_users.complete_quests);
 
 module.exports = router;
