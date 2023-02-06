f5185d4ca2b99c009b8d4ae63967b1a5e6132ef6
ariaalesmana <ariaalesmana@gmail.com>
2023-02-06 12:15:16 +0700

    Update Login Metaverse Bug

--- database/models/users.model.js
+++ database/models/users.model.js
@@ -1,51 +1,48 @@
 module.exports = (sequelize, Sequelize) => {
-    const Users = sequelize.define("tbl_users", {
-        uid: {
-            type: Sequelize.TEXT
-        },
-        id_reg_pd: {
-            type: Sequelize.TEXT
-        },
-        did: {
-            type: Sequelize.TEXT
-        },
-        user: {
-            type: Sequelize.TEXT
-        },
-        pt: {
-            type: Sequelize.TEXT
-        },
-        prodi: {
-            type: Sequelize.TEXT
-        },
-        nim: {
-            type: Sequelize.TEXT
-        },
-        password: {
-            type: Sequelize.TEXT
-        },
-        status: {
-            type: Sequelize.TEXT
-        },
-        wallet_address: {
-            type: Sequelize.TEXT
-        },
-        token: {
-            type: Sequelize.STRING
-        },
-        createdAt: {
-            type: Sequelize.DATE
-        },
-        updatedAt: {
-            type: Sequelize.DATE
-        },
+  const Users = sequelize.define(
+    "tbl_users",
+    {
+      uid: {
+        type: Sequelize.TEXT,
+      },
+      id_reg_pd: {
+        type: Sequelize.TEXT,
+      },
+      did: {
+        type: Sequelize.TEXT,
+      },
+      user: {
+        type: Sequelize.TEXT,
+      },
+      pt: {
+        type: Sequelize.TEXT,
+      },
+      prodi: {
+        type: Sequelize.TEXT,
+      },
+      nim: {
+        type: Sequelize.TEXT,
+      },
+      status: {
+        type: Sequelize.TEXT,
+      },
+      wallet_address: {
+        type: Sequelize.TEXT,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
     },
     {
-        timestamps: false,
-        sequelize,
-        modelName: 'User',
-        tableName: 'tbl_users'
-    });
-  
-    return Users;
+      timestamps: false,
+      sequelize,
+      modelName: "User",
+      tableName: "tbl_users",
+    }
+  );
+
+  return Users;
 };
